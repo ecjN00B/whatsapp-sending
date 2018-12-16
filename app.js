fd0eec7d5845d0309b1a4e09c405f6e9c637cef6
Elias de Carvalho Junior <elias.de.carvalho@ibm.com>
2018-12-16 21:43:32 -0200

    Added socket.io

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var app = require('./config/server'),
     port = process.env.port || 3000;
 
-app.listen(port, () => {
+var server = app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
+});
+
+var io = require('socket.io').listen(server);
+
+io.on('connection', (socket) => {
+    console.log(`${socket.id} connected`);
+    socket.on('disconnect', () => {
+        console.log(`${socket.id} disconnected`);
+    });
 });
